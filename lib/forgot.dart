03af798b1a4d01ea8744636184dc9c1bea363705
ShashwatXD <legendshashwat.gkp@gmail.com>
2024-10-06 18:58:00 +0530

    newwst

--- lib/forgot.dart
+++ lib/forgot.dart
@@ -1,9 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-import 'wrapper.dart';
 
 class Forgot extends StatefulWidget {
   const Forgot({super.key});
